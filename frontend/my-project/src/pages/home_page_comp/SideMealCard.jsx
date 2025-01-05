1958d6c8584ab6228ad6138403d66a7c81f8faf4
May Al Sanea <developer.web.dj@gmail.com>
2025-01-05 05:05:20 +0400

    modify item id design over its image

--- frontend/my-project/src/pages/home_page_comp/SideMealCard.jsx
+++ frontend/my-project/src/pages/home_page_comp/SideMealCard.jsx
@@ -9,26 +9,23 @@ const SideMealCard = ({ smObj }) => {
     console.log("meal id in sideMeal =", smObj.id);
 
     return (
-        // <section>
-        //     <div className="p-2">
-        //         <div className="group relative">
-        //             <img
-        //                 src={smObj.image}
-        //                 alt={smObj.name}
-        //                 className="aspect-[2/3] h-80 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-        //             />
-        //         </div>
-        //     </div>
-        // </section>
+        
         <section className="flex justify-center items-center py-8 md:z-0 z-0">
-                <div className="max-w-sm bg-white rounded-lg shadow-lg overflow-hidden group">
+                <div className="max-w-sm bg-white rounded-lg shadow-lg overflow-hidden group relative">
                         
-                        <div className="absolute mt-2 mx-2 group-hover:scale-105 bg-gray-700 md:z-0 z-0 rounded-full h-fit w-fit p-1">
+                        {/* ID Label */}
+                        {/* <div className="absolute top-4 left-4 bg-gray-700 text-white text-xl font-bold rounded-full h-10 w-10 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <span className="text-3xl text-white">
+                                #{smObj.id}
+                            </span>   
+                        </div> */}
+                        <div className="absolute mt-2 mx-2 group-hover:scale-105 bg-gray-700 md:z-10 z-10 rounded-full h-fit w-fit p-1">
                             <span className="text-3xl text-white">
                                 #{smObj.id}
                             </span>   
                         </div>
                         
+                        {/* Image */}
                         <img
                             src={smObj.image}
                             alt={smObj.name}
@@ -37,7 +34,7 @@ const SideMealCard = ({ smObj }) => {
                         
                         
                        
-
+                   {/* Content */}
                     <div className="relative p-6">
                         <div className="flex items-center justify-center"></div>
                             <div className="relative w-full">
@@ -54,32 +51,34 @@ const SideMealCard = ({ smObj }) => {
                                         {smObj.descriptions}
                                     </p>
                             </div>
-                        </div>    
-                            <div className="p-6 ">        
-                                    <div className="flex justify-around items-center space-x-2 text-xs">
-                                        <div className="bg-gray-200  p-1 text-sm text-gray-700">
-                                            <span className="flex text-xs items-center">
-                                                <img src={weightSvg} className="h-6 w-6 mr-1 items-center"/>
-                                                <span className="font-semibold">{smObj.size}</span>
-                                            </span>
-                                        </div>
-                                        <div className="bg-gray-200  p-1 text-sm text-gray-700">
-                                            <span className="flex text-xs items-center">
-                                                <img src={kcalSvg}  className="h-6 w-6 mr-1 "/>
-                                                <span className="font-semibold">{smObj.caloriesPerServing}</span>
-                                            </span>
-                                        </div>
-                                    </div>
+                        </div>
 
-                                    <br></br>
-
-                                    <div className="items-center bg-white text-xs">    
-                                        <span className="flex text-s items-center">
-                                            <img src="https://img.freepik.com/free-psd/star-winner-rating-review-icon-sign-symbol-3d-background-illustration_56104-2417.jpg"  className="h-8 w-8 mr-1 items-center"/>
-                                            <span className="font-semibold">{smObj.rating}</span>({smObj.reviewCount})
+                        {/* Additional Info */}
+                        <div className="p-6 ">        
+                                <div className="flex justify-around items-center space-x-2 text-xs">
+                                    <div className="bg-gray-200  p-1 text-sm text-gray-700">
+                                        <span className="flex text-xs items-center">
+                                            <img src={weightSvg} className="h-6 w-6 mr-1 items-center"/>
+                                            <span className="font-semibold">{smObj.size}</span>
                                         </span>
                                     </div>
-                            </div>
+                                    <div className="bg-gray-200  p-1 text-sm text-gray-700">
+                                        <span className="flex text-xs items-center">
+                                            <img src={kcalSvg}  className="h-6 w-6 mr-1 "/>
+                                            <span className="font-semibold">{smObj.caloriesPerServing}</span>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <br></br>
+
+                                <div className="items-center bg-white text-xs">    
+                                    <span className="flex text-s items-center">
+                                        <img src="https://img.freepik.com/free-psd/star-winner-rating-review-icon-sign-symbol-3d-background-illustration_56104-2417.jpg"  className="h-8 w-8 mr-1 items-center"/>
+                                        <span className="font-semibold">{smObj.rating}</span>({smObj.reviewCount})
+                                    </span>
+                                </div>
+                        </div>
                      
                 </div>
         </section>
